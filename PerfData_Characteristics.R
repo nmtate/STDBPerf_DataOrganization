00b9fa6322820c0fa500e010aee97b805efc14d0
nmtate <nmtate@umn.edu>
2023-12-05 08:54:35 -0600

    commit perfdata_characteristics code

---
+++ PerfData_Characteristics.R
@@ -0,0 +1,4 @@
+basic_df <- read.csv("/Users/nmtate/Library/CloudStorage/Dropbox/1EquineProjects/STDB_Projects/Data/PerformanceData/data/STDBPerf_Basic_onlyraced_12052023.csv")
+annual_df <- read.csv("/Users/nmtate/Library/CloudStorage/Dropbox/1EquineProjects/STDB_Projects/Data/PerformanceData/data/STDBPerf_Annual_onlyraced_12052023.csv")
+race_df <- read.csv("/Users/nmtate/Library/CloudStorage/Dropbox/1EquineProjects/STDB_Projects/Data/PerformanceData/data/STDBPerf_Raceline_onlyraced_12052023.csv")
+
